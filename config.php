97990e91d3983b0cb65fe036f6060b9521a4e0eb
EduardoDArellano <eduardodarellano@gmail.com>
2018-06-18 21:55:48 -0500

    Power Paint Solutions
    Página web powerpaintsolutions.com

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+//Contact form config
+//$email_to = "support@themeleaf.com";
+$email_to = "admin@ssmty.mx";
+$email_subject = "Contacto de cliente";
+
+//recaptcha config
+$use_recaptcha = true;
+$publickey = "6LeMXt4SAAAAABbnTq7FvelG2GANlmpl_sWvWP0b";
+$privatekey = "6LeMXt4SAAAAAFZ7CGFwmppH5bD2W1gA1Nvpflvs";
+?>
